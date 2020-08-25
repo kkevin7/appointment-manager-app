fbd002cfe1227d906834a6bf61bebd2f65d8c901
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-25 10:25:44 -0600

    using components similiar to react

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import {
   StyleSheet,
   Text,
+  View,
 } from 'react-native';
 
 const App = () => {
+
+  const [citas, setCitas] = useState([
+    {id: "1", paciente: "Hook", propietario: 'Juan', sintomas: "No Come"},
+    {id: "2", paciente: "Redux", propietario: 'Itzel', sintomas: "No Duerme"},
+    {id: "3", paciente: "Native", propietario: 'Josue', sintomas: "No Canta"},
+  ]);
+
   return (
-      <Text style={styles.title}>Proyectos Citas</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>Administador de Citas</Text>
+        {citas.map(cita => (
+          <View>
+            <Text >{cita.paciente}</Text>
+          </View>
+        ))}
+      </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#AA076B',
+    flex: 1,
+
+  },
   title:{
+    marginTop: 40,
+    fontSize: 24,
+    fontWeight: 'bold',
     textAlign: 'center',
-    marginTop: 100
-  }
+  },
+  
 });
 
 export default App;
