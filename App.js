2896f25656a34390fcb4aec3ddb03af8d5dc3aef
Kevin Martinez <kkevinmartinez7@gmail.com>
2020-08-25 11:27:15 -0600

    Deleting elements

--- App.js
+++ App.js
@@ -1,31 +1,35 @@
-import React ,{useState} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-} from 'react-native';
-import Cita from "./components/Cita";
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+import Cita from './components/Cita';
 
 const App = () => {
-
   const [citas, setCitas] = useState([
-    {id: "1", paciente: "Hook", propietario: 'Juan', sintomas: "No Come"},
-    {id: "2", paciente: "Redux", propietario: 'Itzel', sintomas: "No Duerme"},
-    {id: "3", paciente: "Native", propietario: 'Josue', sintomas: "No Canta"},
+    {id: '1', paciente: 'Hook', propietario: 'Juan', sintomas: 'No Come'},
+    {id: '2', paciente: 'Redux', propietario: 'Itzel', sintomas: 'No Duerme'},
+    {id: '3', paciente: 'Native', propietario: 'Josue', sintomas: 'No Canta'},
   ]);
 
+  //Elimina los pacientes del state
+  const eliminarPaciente = (id) => {
+    setCitas((citasActuales) => citasActuales.filter((cita) => cita.id !== id));
+  };
+
+  console.log(citas);
+
   return (
-      <View style={styles.container}>
-        <Text style={styles.title}>Administador de Citas</Text>
-        <FlatList 
-          data={citas}
-          renderItem={({item}) => (
-            <Cita item={item} ></Cita>
-          )}
-          keyExtractor={cita => cita.id}
-        />
-      </View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Administador de Citas</Text>
+      <Text style={styles.title}>
+        {citas.length > 0 ? 'Administra tus Citas' : 'No hay citas, agrega una'}
+      </Text>
+      <FlatList
+        data={citas}
+        renderItem={({item}) => (
+          <Cita item={item} eliminarPaciente={() => eliminarPaciente(item.id)}></Cita>
+        )}
+        keyExtractor={(cita) => cita.id}
+      />
+    </View>
   );
 };
 
@@ -34,13 +38,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#AA076B',
     flex: 1,
   },
-  title:{
+  title: {
     marginTop: 40,
+    marginBottom: 20,
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
   },
-  
 });
 
 export default App;
--- components/Cita.js
+++ components/Cita.js
@@ -1,63 +1,65 @@
 import React from 'react';
 import {Text, StyleSheet, View, Button, TouchableHighlight} from 'react-native';
 
-const Cita = ({item}) => {
-
-    const dialogoEliminar = () => {
-        console.log('eliminando...');
-    }
+const Cita = ({item, eliminarPaciente}) => {
+  const dialogoEliminar = () => {
+    console.log('eliminando...');
+    eliminarPaciente();
+  };
 
   return (
-        <View style={styles.cita}>
-            <View>
-                <Text style={styles.label} >Paciente: </Text>
-                <Text style={styles.texto}>{item.paciente}</Text>
-            </View>
-            <View>
-                <Text style={styles.label} >Paciente: </Text>
-                <Text style={styles.texto}>{item.propietario}</Text>
-            </View>
-            <View>
-                <Text style={styles.label} >Paciente: </Text>
-                <Text style={styles.texto}>{item.sintomas}</Text>
-            </View>
+    <View style={styles.cita}>
+      <View>
+        <Text style={styles.label}>Paciente: </Text>
+        <Text style={styles.texto}>{item.paciente}</Text>
+      </View>
+      <View>
+        <Text style={styles.label}>Paciente: </Text>
+        <Text style={styles.texto}>{item.propietario}</Text>
+      </View>
+      <View>
+        <Text style={styles.label}>Paciente: </Text>
+        <Text style={styles.texto}>{item.sintomas}</Text>
+      </View>
 
-            <View>
-                <TouchableHighlight style={styles.btnEliminar} onPress={ () => dialogoEliminar()}>
-                    <Text style={styles.textEliminar}>Eliminar &times;</Text>
-                </TouchableHighlight>
-            </View>
-        </View>
+      <View>
+        <TouchableHighlight
+          style={styles.btnEliminar}
+          onPress={() => dialogoEliminar()}>
+          <Text style={styles.textEliminar}>Eliminar &times;</Text>
+        </TouchableHighlight>
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-    cita: {
-        backgroundColor: '#FFF',
-        borderBottomColor: "#e1e1e1",
-        borderStyle: 'solid',
-        borderBottomWidth: 1,
-        paddingVertical: 20,
-        paddingHorizontal: 10,
-    },
-    label:{
-        fontWeight: 'bold',
-        fontSize: 18,
-        marginTop: 5,
-    },
-    texto: {
-        fontSize: 18,
-    },
-    btnEliminar: {
-        padding: 10,
-        backgroundColor: 'red',
-        marginVertical: 10,
-    },
-    textEliminar: {
-        color: '#FFF',
-        fontWeight: 'bold',
-        textAlign: 'center',
-    }
+  cita: {
+    backgroundColor: '#FFF',
+    borderBottomColor: '#e1e1e1',
+    borderStyle: 'solid',
+    borderBottomWidth: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginTop: 5,
+  },
+  texto: {
+    fontSize: 18,
+  },
+  btnEliminar: {
+    padding: 10,
+    backgroundColor: 'red',
+    marginVertical: 10,
+  },
+  textEliminar: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
 
 export default Cita;
